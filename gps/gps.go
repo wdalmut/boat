129ec71290a27904338ea080ba2f1dbce48650d7
Walter Dal Mut <walter.dalmut@gmail.com>
2013-11-17 17:03:21 +0100

    Included C math library

--- gps/gps.go
+++ gps/gps.go
@@ -1,9 +1,17 @@
 package gps
 
 import(
-    // #cgo LDFLAGS: -lgps
+    // #cgo LDFLAGS: -lgps -lm
     // #include <gps.h>
     "C"
+    "math"
+)
+
+const (
+    Earth = 6371
+    DegToRad = 180/math.Pi
+    RadToDeg = math.Pi/180
+
 )
 
 func GpsLocation() (float64, float64) {
@@ -16,4 +24,18 @@ func GpsLocation() (float64, float64) {
     lon = float64(data.longitude)
 
     return lat, lon
+}
+
+func Distance(lat1, lon1, lat2, lon2 float64) float64 {
+    dLat := (lat2 - lat1) * DegToRad
+    dLon := (lon2 - lon1) * DegToRad
+
+    lat1 = lat1 * DegToRad
+    lat2 = lat2 * DegToRad
+
+    a := math.Sin(dLat/2) * math.Sin(dLat/2) + math.Sin(dLon/2) * math.Sin(dLon/2) * math.Cos(lat1) * math.Cos(lat2)
+    c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a));
+
+    d := Earth * c;
+    return d;
 }

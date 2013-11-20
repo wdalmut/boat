0543b7639b9e41084ab24e05b5f4837ef12baf98
Walter Dal Mut <walter.dalmut@gmail.com>
2013-11-20 08:30:13 +0100

    Added bearing computation

--- geo/point.go
+++ geo/point.go
@@ -43,3 +43,15 @@ func Distance(point1, point2 *Point) float64 {
         math.Cos(point2.Longitude *DegToRad - point1.Longitude * DegToRad)) * Earth
 }
 
+// Calculate the angle between 2 points
+func Bearing(point1, point2 *Point) float64 {
+    dLon := (point2.Longitude - point1.Longitude) * DegToRad
+    lat1 := point1.Latitude * DegToRad
+    lat2 := point2.Latitude * DegToRad
+
+    y := math.Sin(dLon) * math.Cos(lat2);
+    x := math.Cos(lat1) * math.Sin(lat2) - math.Sin(lat1) * math.Cos(lat2)*math.Cos(dLon)
+
+    return math.Atan2(y, x) * RadToDeg
+}
+
--- geo/point_test.go
+++ geo/point_test.go
@@ -44,6 +44,22 @@ func TestDistance(t *testing.T) {
     }
 }
 
+func TestBearing(t *testing.T) {
+    point1 := new(Point)
+    point1.Latitude = 43.91
+    point1.Longitude = 8.1125
+
+    point2 := new(Point)
+    point2.Latitude = 44.055556
+    point2.Longitude = 9.816944
+
+    want := 82.6402
+
+    if angle := Bearing(point1, point2); math.Abs(angle - want) > 0.001 {
+        t.Errorf("Wrong distance: %v, want %v", angle, want)
+    }
+}
+
 func BenchmarkMidpoint(b *testing.B) {
     point1 := new(Point)
     point1.Latitude = 50.066389
@@ -71,4 +87,19 @@ func BenchmarkDistance(b *testing.B) {
         Distance(point1, point2)
     }
 }
+
+func BenchmarkBearing(b *testing.B) {
+    point1 := new(Point)
+    point1.Latitude = 50.066389
+    point1.Longitude = 5.714722
+
+    point2 := new(Point)
+    point2.Latitude = 58.643889
+    point2.Longitude = 3.07
+
+    for i := 0; i < b.N; i++ {
+        Bearing(point1, point2)
+    }
+}
+
 
